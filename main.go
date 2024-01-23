9b32fcccc51dd7a3408de5617f90e58f41d52892
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-01-23 17:17:54 -0300

    :building_construction: added a .env file to set the server port

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	StartServer(":3000")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Print("Could not read `.env` file")
+		log.Panic(err)
+	}
+
+	port := os.Getenv("PORT")
+
+	StartServer(":" + port)
 }
