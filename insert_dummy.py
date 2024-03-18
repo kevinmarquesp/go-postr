2d929570ef123d20e59b34a1b3d6beb2ef96ff8e
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-18 14:41:37 -0300

    :construction: wip (mock): now the insert_dummy.py script can load the .env variables too

--- insert_dummy.py
+++ insert_dummy.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import os
+import re
 from requests import get
 from json import loads
 
 DUMMYUSERS_API: str = "https://jsonplaceholder.org/users"
+DOTENV_FILE = ".env"
 
 
 def insert_user_credentials(user):
@@ -17,5 +20,18 @@ def main() -> None:
     print("\n[TODO]: Insert user credentials to database\n")
 
 
+def load_dotenv(dotenv_file):
+    dotenv = []
+
+    with open(dotenv_file, "r") as file:
+        dotenv = re.findall(r"^\s*([^#\s][^=]+)\s*=\s*(.*)\s*$", file.read(),
+                            flags=re.MULTILINE)
+
+    for key, value in dotenv:
+        os.environ[key] = value
+
+
 if __name__ == "__main__":
+    load_dotenv(DOTENV_FILE)
+
     main()
