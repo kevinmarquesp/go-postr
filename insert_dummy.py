16e4ab0b2fd136fe02bebfd8caf4d3cbe25566dc
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-18 14:43:53 -0300

    :construction: wip (mock): now it also accepts command line options arguments to connect to a db server

--- insert_dummy.py
+++ insert_dummy.py
@@ -4,8 +4,10 @@ import os
 import re
 from requests import get
 from json import loads
+from sys import argv
+from argparse import ArgumentParser
 
-DUMMYUSERS_API: str = "https://jsonplaceholder.org/users"
+DUMMYUSERS_API = "https://jsonplaceholder.org/users"
 DOTENV_FILE = ".env"
 
 
@@ -13,11 +15,42 @@ def insert_user_credentials(user):
     pass
 
 
-def main() -> None:
+def main(args) -> None:
     r = get(DUMMYUSERS_API)
     _ = loads(r.text)  # users
 
-    print("\n[TODO]: Insert user credentials to database\n")
+    print(args)
+
+
+def parse_args(user_args):
+    parser = ArgumentParser(description="insert dummy users information in a\
+                            postgres database connection, by default it will\
+                            read the environment variables if any argument was\
+                            specified")
+
+    parser.add_argument("-u", "--username", type=str,
+                        default=os.getenv("POSTGRES_USER"),
+                        help="username credential info, to login")
+
+    parser.add_argument("-p", "--password",
+                        type=str, default=os.getenv("POSTGRES_PASSWORD"),
+                        help="password credential info")
+
+    parser.add_argument("-P", "--port", type=int,
+                        default=os.getenv("POSTGRES_PORT"),
+                        help="database connection port number")
+
+    parser.add_argument("-H", "--host", type=str,
+                        default=os.getenv("POSTGRES_HOST"),
+                        help="hostname of the database server, use 'localhost'\
+                        for development environment")
+
+    parser.add_argument("-d", "--database", type=str,
+                        default=os.getenv("POSTGRES_DB"),
+                        help="default database name, where the application\
+                        information is stored")
+
+    return parser.parse_args(user_args)
 
 
 def load_dotenv(dotenv_file):
@@ -34,4 +67,6 @@ def load_dotenv(dotenv_file):
 if __name__ == "__main__":
     load_dotenv(DOTENV_FILE)
 
-    main()
+    args = parse_args(argv[1:])
+
+    main(args)
