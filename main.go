543c0c5073ab189c21344205a5e4a6b88b3cf03a
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-19 12:54:38 -0300

    :sparkles: feature!: now the /search/user is working just fine with the database connection

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"go-postr/db"
 	"go-postr/templates"
 	"net/http"
 	"os"
@@ -17,8 +19,39 @@ func router(port string) {
 
 	http.HandleFunc("/search/user", func(w http.ResponseWriter, r *http.Request) {
 		v := r.URL.Query()
+		query := v.Get("query")
 
-		log.Info(v.Get("query"))
+		if len(query) == 0 {
+			fmt.Fprintf(w, "")  // insert an empty string in the results tag
+			return
+		}
+
+		rows, err := db.Connection().Query(`SELECT username FROM "user" WHERE username LIKE $1`,
+			"%" + query + "%")
+		if err != nil {
+			log.Error("Could not fetch usernames like " + query, "error", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+
+			return
+		}
+
+		list := ""
+
+		for rows.Next() {
+			var username string
+
+			err = rows.Scan(&username)
+			if err != nil {
+				log.Error("Could not scan db column...", "error", err)
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+
+				return
+			}
+
+			list += fmt.Sprintf(`<li><a href="/u/%s">%s</a></li>`, username, username)
+		}
+
+		fmt.Fprintf(w, list)
 	})
 
 	log.Info("Listening", "url", "http://localhost" + port)
