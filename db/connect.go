0aba8f6f5c3d41d6b20014ffbc2116d85cc62e50
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-19 12:48:00 -0300

    :sparkles: feature: created a database module that connect and saves it in a global connection var

---
+++ db/connect.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type ConnCredentials struct {
+	Host			string
+	Port			string
+	Username		string
+	Password		string
+	DatabaseName	string
+}
+
+var conn *sql.DB
+
+func Connection() *sql.DB {
+	return conn
+}
+
+func Connect(cred ConnCredentials) error {
+	credentials := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cred.Host, cred.Port, cred.Username, cred.Password, cred.DatabaseName)
+
+	var err error
+
+	conn, err = sql.Open("postgres", credentials)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
