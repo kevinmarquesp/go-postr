e25090e37e922ab20a034eb38242744f9d1c6bcb
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-19 08:58:20 -0300

    :sparkles: feature!: added a TemplateRenderer struct to make the front-end stuff easier to handle

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io"
 	"net/http"
 )
 
+const parseFilesLocation = "html/*.html"
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplateRenderer() *TemplateRenderer {
+	return &TemplateRenderer{
+		templates: template.Must(template.ParseGlob(parseFilesLocation)),
+	}
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world!")
+		templ := NewTemplateRenderer()
+
+		templ.Render(w, "Index", nil)
 	})
 
 	http.ListenAndServe(":8000", nil)
