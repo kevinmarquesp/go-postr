3f4d284dabd5bff8b2539f3d93eba4c67b14e5b6
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-19 09:20:20 -0300

    :recycle: refactoring: created a router function to handle the routes, now the main is free for setup

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"os"
 )
 
+func router(port string) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		templ := templates.NewTemplateRenderer()
+
+		templ.Render(w, "Index", nil)
+	})
+
+	http.ListenAndServe(port, nil)
+}
 
 func main() {
 	port := ":" + os.Getenv("PORT")
@@ -14,11 +23,5 @@ func main() {
 		port = ":8000"
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		templ := templates.NewTemplateRenderer()
-
-		templ.Render(w, "Index", nil)
-	})
-
-	http.ListenAndServe(port, nil)
+	router(port)
 }
