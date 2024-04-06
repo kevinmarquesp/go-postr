49053aa76436058266ede98dcd73096632279c91
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-04-06 13:41:23 -0300

    :rewind: revert: using implicit GET method for most of the routes

--- router/router.go
+++ router/router.go
@@ -7,15 +7,15 @@ import (
 )
 
 func InitRouter(port string) {
-	http.Handle("GET /js/", http.StripPrefix("/js/", http.FileServer(http.Dir("www/js"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("www/js"))))
 
 	http.HandleFunc("/", redirectToHomePage)
-	http.HandleFunc("GET /home", renderIndexView)
-	http.HandleFunc("GET /signup", renderSignupView)
+	http.HandleFunc("/home", renderIndexView)
+	http.HandleFunc("/signup", renderSignupView)
 
-	http.HandleFunc("GET /search/user", searchUsernameController)
-	http.HandleFunc("GET /get/articles", getRecentArticlesController)
-	http.HandleFunc("GET /validate/username", usernameValidationController)
+	http.HandleFunc("/search/user", searchUsernameController)
+	http.HandleFunc("/get/articles", getRecentArticlesController)
+	http.HandleFunc("/validate/username", usernameValidationController)
 	http.HandleFunc("POST /insert/user", createNewUserController)
 
 	log.Info("Listening to", "url", "http://localhost" + port)
