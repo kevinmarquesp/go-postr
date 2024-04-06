4ca4e30632b35e413a598757428a8e9a895e762a
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-04-06 15:58:03 -0300

    :rewind: revert: aparently I need to add the GET method even for the /js/ route to make the / work

--- router/router.go
+++ router/router.go
@@ -7,14 +7,14 @@ import (
 func InitRouter(port string) {
 	app := http.NewServeMux()
 
-	app.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("www/js"))))
+	app.Handle("GET /js/", http.StripPrefix("/js/", http.FileServer(http.Dir("www/js"))))
 
-	app.HandleFunc("/", renderIndexView)
-	app.HandleFunc("/signup", renderSignupView)
+	app.HandleFunc("GET /", renderIndexView)
+	app.HandleFunc("GET /signup", renderSignupView)
 
-	app.HandleFunc("/api/search/user", searchUsernameController)
-	app.HandleFunc("/api/get/articles", getRecentArticlesController)
-	app.HandleFunc("/api/validate/username", usernameValidationController)
+	app.HandleFunc("GET /api/search/user", searchUsernameController)
+	app.HandleFunc("GET /api/get/articles", getRecentArticlesController)
+	app.HandleFunc("GET /api/validate/username", usernameValidationController)
 	app.HandleFunc("POST /api/insert/user", createNewUserController)
 
 	server := http.Server{
