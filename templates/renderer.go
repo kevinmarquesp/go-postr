79c345d65ddec287220173415178084ba268151a
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-04-12 15:34:33 -0300

    :bulb: docs-code: simple comments to the templates package

--- templates/renderer.go
+++ templates/renderer.go
@@ -12,10 +12,16 @@ type TemplateRenderer struct {
 	templates *template.Template
 }
 
+// Render the HTML output in the response writter. The `name` is the name of
+// the template defined with `{{ define "TEMPLATE_NAME" }}` inside the `html`
+// directory.
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+// Will return the generated string basing on the template files The `name` is
+// the name of the template defined with `{{ define "TEMPLATE_NAME" }}` inside
+// the `html` directory.
 func (t *TemplateRenderer) RenderString(name string, data interface{}) (string, error) {
 	var buf bytes.Buffer
 
@@ -27,9 +33,10 @@ func (t *TemplateRenderer) RenderString(name string, data interface{}) (string, 
 	return buf.String(), nil
 }
 
+// Build a new TemplateRenderer object, it will use all the HTML files inside
+// the `html/` directory by default - be aware of that.
 func NewTemplateRenderer() *TemplateRenderer {
 	return &TemplateRenderer{
 		templates: template.Must(template.ParseGlob(parseFilesLocation)),
 	}
 }
-
