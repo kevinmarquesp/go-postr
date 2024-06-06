b4549ee2c62236c5fa2b32603af57359ba62d559
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-06 17:03:11 -0300

    :construction: wip: added tailwindcss as a node package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,12 +9,14 @@ import (
 func main() {
 	app := http.NewServeMux()
 
+	app.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	app.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		home.Home().Render(r.Context(), w)
 	})
 
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: app,
 	}
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./views/**/*.templ"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
