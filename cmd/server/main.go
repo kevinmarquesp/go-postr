bac2d26f0c4e3590570bff13dbbfef90a217b40b
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-05 22:43:41 -0300

    :construction: wip: basic hello world server to try everything out

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	app := http.NewServeMux()
+
+	app.HandleFunc("GET /", func(w http.ResponseWriter, _r *http.Request) {
+		fmt.Fprint(w, "<h1>Hello world! From Go 1.22.1</h1>")
+	})
+
+	server := http.Server{
+		Addr: ":8080",
+		Handler: app,
+	}
+
+	server.ListenAndServe()
 }
