ea01f135dc75ebfda887420959f45cfc5d3e55da
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-07 01:18:38 -0300

    :card_file_box: db (migration): first migration created, this one creates the basic users table to start with

---
+++ db/migrations/20240607035541_adding_users_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	inserted_at TIMESTAMP(0) NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc'),
+	updated_at TIMESTAMP(0) NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+-- +goose StatementEnd
