b8c6f180a82cdbdcbe8c2fba70f8f3b99748c6dc
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-08 17:48:50 -0300

    :card_file_box: db (update): added a description field to the users table migration

--- db/migrations/20240607035541_adding_users_table.sql
+++ db/migrations/20240607035541_adding_users_table.sql
@@ -4,7 +4,8 @@ CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	username VARCHAR(255) UNIQUE NOT NULL,
 	password VARCHAR(255) NOT NULL,
-	inserted_at TIMESTAMP(0) NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc'),
+	description TEXT,
+	created_at TIMESTAMP(0) NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc'),
 	updated_at TIMESTAMP(0) NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
 );
 -- +goose StatementEnd
