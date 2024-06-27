06a1da1d04fe9c283c05c5a6cfe42072ac07a8e0
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-27 19:21:36 -0300

    :sparkles: feature: Created a middleware function to run custom code and log the request information

---
+++ cmd/server/middleware.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/charmbracelet/log"
+)
+
+// Wrapper to hold the writer status when updating the status code.
+type StatusWriter struct {
+	http.ResponseWriter
+
+	status int
+}
+
+func (sw *StatusWriter) WriteHeader(updatedStatus int) {
+	sw.ResponseWriter.WriteHeader(updatedStatus)
+
+	sw.status = updatedStatus
+}
+
+func MiddlewareHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		timerStart := time.Now()
+
+		sw := &StatusWriter{
+			ResponseWriter: w,
+			status:         http.StatusOK,
+		}
+
+		if Middleware(sw, r) {
+			handler.ServeHTTP(sw, r)
+		}
+
+		log.Printf("%s\t%d %s\t%s", time.Since(timerStart), sw.status, r.Method, r.URL.Path)
+	})
+}
+
+func Middleware(sw http.ResponseWriter, r *http.Request) bool {
+	log.Info("Nuh uh.")
+	sw.WriteHeader(http.StatusForbidden)
+
+	return false
+}
