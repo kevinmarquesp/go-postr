86bd9c1b6daf58fae696c776ffac158247b1a4fc
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-27 14:09:12 -0300

    :sparkles: feature: Now loading the .env file, if it exists

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/charmbracelet/log"
+	"github.com/joho/godotenv"
+)
+
+const DOTENV = ".env"
 
 func main() {
-	fmt.Println("Hello world!")
+	log.Info("Initializing the Go Postr application...")
+
+	if err := godotenv.Load(DOTENV); err != nil {
+		log.Warn("Could not load the" + DOTENV + " file, using the system's environment.")
+	}
+
+	log.Print("Hello world!")
 }
