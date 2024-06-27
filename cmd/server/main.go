84cdce33bb0567cd2157239ed6f75da9853ac494
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-27 15:19:04 -0300

    :clipboard: impl: Now the main() function holds a unused database service struct variable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"os"
+
 	"github.com/charmbracelet/log"
 	"github.com/joho/godotenv"
+
+	"github.com/kevinmarquesp/go-postr/internal/models"
 )
 
 const DOTENV = ".env"
@@ -12,6 +16,14 @@ func main() {
 
 	if err := godotenv.Load(DOTENV); err != nil {
 		log.Warn("Could not load the" + DOTENV + " file, using the system's environment.")
+	}
+
+	db := &models.Sqlite{}
+
+	err := db.Connect(os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		log.Fatal("Could not connect to the database.", "err", err)
 	}
 
 	log.Print("Hello world!")
