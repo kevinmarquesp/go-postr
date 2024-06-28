ee6554ea0f192de23a17ab9ac9e611c1709ac2a8
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-27 23:59:48 -0300

    :art: style (api): Now constructing the JSON error response with a struct, instead of just a string

--- internal/utils/writer.go
+++ internal/utils/writer.go
@@ -1,13 +1,24 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/kevinmarquesp/go-postr/internal/data"
 )
 
 func WriteJsonError(w http.ResponseWriter, status int, err error) {
 	w.WriteHeader(status)
 
-	fmt.Fprintf(w, `{ "status": %d, "statusText": "%s", "error": "%s" }`,
-		status, http.StatusText(status), err)
+	jsonError := &data.ErrorResponse{
+		Status:     status,
+		StatusText: http.StatusText(status),
+		Error:      err.Error(),
+	}
+
+	errorBytes, _ := json.Marshal(jsonError)
+	errorString := string(errorBytes)
+
+	fmt.Fprint(w, errorString)
 }
