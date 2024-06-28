c26285e894c5413cd3f965ca98659bee1b2053da
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-28 10:31:34 -0300

    :sparkles: feature: Created a new data struct for registration responses

--- internal/data/auth.go
+++ internal/data/auth.go
@@ -5,3 +5,8 @@ type RegisterCredentialsIncome struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+type RegisterSuccessfulSessionTokenResponse struct {
+	Username     string `json:"username"`
+	SessionToken string `json:"sessionToken"`
+}
