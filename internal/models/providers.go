6c87d5b9aebc347bfc8278124be04b457a64de7e
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-28 20:43:02 -0300

    :bug: fix (models): Now the generic DatabaseProvider has the updated version of the auth method...

--- internal/models/providers.go
+++ internal/models/providers.go
@@ -3,4 +3,5 @@ package models
 type DatabaseProvider interface {
 	Connect(url string) error
 	RegisterNewUser(username string, password string) (string, error)
+	AuthorizeUserWithSessionToken(username string, sessionToken string) (string, error)
 }
