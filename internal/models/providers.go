323678bbbadc7c7aec8652c037985cdc72d6aab1
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-28 10:01:24 -0300

    :sparkles: feature (models): Created a function to insert a new user and return the generated session token

--- internal/models/providers.go
+++ internal/models/providers.go
@@ -2,4 +2,5 @@ package models
 
 type DatabaseProvider interface {
 	Connect(url string) error
+	RegisterNewUser(username string, password string) (string, error)
 }
--- internal/models/sqlite.go
+++ internal/models/sqlite.go
@@ -1,9 +1,19 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/hex"
+	"time"
 
+	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	BCRYPT_COST     = bcrypt.MinCost
+	SESSION_EXPIRES = 30 * time.Second
 )
 
 type Sqlite struct {
@@ -19,4 +29,33 @@ func (s *Sqlite) Connect(url string) error {
 	s.conn = conn
 
 	return nil
+}
+
+func (s *Sqlite) RegisterNewUser(username string, password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(username+password), BCRYPT_COST)
+	if err != nil {
+		return "", err
+	}
+
+	token := GenerateSessionToken(username)
+	expirationDate := time.Now().Add(SESSION_EXPIRES)
+
+	_, err = s.conn.Query(`INSERT INTO users (username, password, session_token, session_expires)
+        VALUES ($1, $2, $3, $4)`, username, hashedPassword, token, expirationDate)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func GenerateSessionToken(username string) string {
+	shaAlgorithm := sha256.New()
+
+	shaAlgorithm.Write([]byte(username))
+
+	hashedUsername := hex.EncodeToString(shaAlgorithm.Sum(nil))
+	tokenId := uuid.New().String()
+
+	return hashedUsername[:12] + "." + tokenId
 }
