c32974adf40914b65930a330e3f3b3b78aadeb05
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-07-02 15:08:31 -0300

    :bulb: docs-code (models): Added a little documentation for the register function, it has too much returns

--- internal/models/providers.go
+++ internal/models/providers.go
@@ -18,7 +18,12 @@ const (
 
 type GenericDatabaseProvider interface {
 	Connect(url string) error
+
+	// This function will return the public ID of the inserted user and its
+	// session token ID (which has an expiration date defined by the
+	// models.SESSION_MAX_DURATION constant)
 	RegisterNewUser(fullname, username, password string) (string, string, error)
+
 	AuthorizeUserWithSessionToken(sessionToken string) (string, error)
 	AuthorizeUserWithCredentials(username, password string) (string, error)
 }
