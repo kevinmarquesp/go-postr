d1473ec88cdda2ad70eb9d3a0eb3b26eb3275b28
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-07-04 20:02:15 -0300

    :fire: prune (data): Removed the old data structs that I'm not using anywere anymore

--- internal/data/auth.go
+++ internal/data/auth.go
@@ -1,19 +1,5 @@
 package data
 
-// Used on the user register API related routes.
-
-type RegisterNewUserBodyCredentialsBody struct {
-	Fullname string `json:"fullname"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-type RegisterNewUserSuccessfulResponse struct {
-	Username     string `json:"username"`
-	PublicID     string `json:"publicId"`
-	SessionToken string `json:"sessionToken"`
-}
-
 // Used in the refresh session token related routes.
 
 type RefreshUserSessionTokenCredentialsBody struct {
