458e43e488b756794199c97331a809e133590d0e
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-07-18 22:56:57 -0300

    :white_check_mark: test (services.authentication): Now testing the basic usage of the authenticate mechanism

--- internal/services/test/service_authentication_test.go
+++ internal/services/test/service_authentication_test.go
@@ -1,7 +1,9 @@
 package services_test
 
 import (
+	"errors"
 	"testing"
+	"time"
 
 	"github.com/kevinmarquesp/go-postr/internal/repositories"
 	"github.com/kevinmarquesp/go-postr/internal/services"
@@ -10,11 +12,60 @@ import (
 
 type MockedUserRepository struct {
 	repositories.UserRepository
+
+	MockFailCreateNewUser bool
 }
+
+func (mur MockedUserRepository) CreateNewUser(name, username, email, password string) (repositories.UserSchema, error) {
+	empty := repositories.UserSchema{}
+
+	if mur.MockFailCreateNewUser {
+		return empty, errors.New("System under test: Forced fail.")
+	}
+
+	return repositories.UserSchema{
+		Name:      name,
+		Username:  username,
+		Email:     email,
+		Password:  password,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}, nil
+}
+
+// The test units starts here!
 
 func TestNewGopostrAuthentication(t *testing.T) {
 	mockUserRepo := MockedUserRepository{}
 	authenticationService := services.NewGopostrAuthenticationService(mockUserRepo)
 
 	assert.IsType(t, services.GopostrAuthenticationService{}, authenticationService)
+}
+
+func TestAuthenticateWithCredentials(t *testing.T) {
+	t.Run("Should fail with invalid credentials.", func(t *testing.T) {
+		mockUserRepo := MockedUserRepository{}
+		authenticationService := services.NewGopostrAuthenticationService(mockUserRepo)
+
+		_, err := authenticationService.AuthenticateWithCredentials("Fulano", "user", "email.com", "password", "password")
+		assert.Error(t, err)
+	})
+
+	t.Run("Should fail with wrong passwords.", func(t *testing.T) {
+		mockUserRepo := MockedUserRepository{}
+		authenticationService := services.NewGopostrAuthenticationService(mockUserRepo)
+
+		_, err := authenticationService.AuthenticateWithCredentials("Fulano de Tal", "fulano", "me@email.com", "Password123!", "Password123$")
+		assert.Error(t, err)
+	})
+
+	t.Run("Should create a user with a hashed password with bcrypt.", func(t *testing.T) {
+		mockUserRepo := MockedUserRepository{}
+		authenticationService := services.NewGopostrAuthenticationService(mockUserRepo)
+
+		createdUser, err := authenticationService.AuthenticateWithCredentials("Fulano de Tal", "fulano", "me@email.com", "Password123!", "Password123!")
+		assert.NoError(t, err)
+
+		assert.Len(t, createdUser.Password, 60)
+	})
 }
