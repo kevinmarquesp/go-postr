fa4142d68f897f1b45bb08d825e937476c6301ac
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-07-18 22:15:39 -0300

    :clipboard: impl (services.authentication): Now this service can sign a new user to the user's repository, in theory

--- internal/services/service_authentication.go
+++ internal/services/service_authentication.go
@@ -1,6 +1,14 @@
 package services
 
-import "github.com/kevinmarquesp/go-postr/internal/repositories"
+import (
+	"errors"
+	"os"
+	"strings"
+
+	"github.com/kevinmarquesp/go-postr/internal/models"
+	"github.com/kevinmarquesp/go-postr/internal/repositories"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type GopostrAuthentication struct {
 	AuthenticationService
@@ -17,5 +25,57 @@ func NewGopostrAuthentication(userRepo repositories.UserRepository) GopostrAuthe
 func (ga GopostrAuthentication) AuthenticateWithCredentials(name, username, email, password, confirmation string) (repositories.UserSchema, error) {
 	empty := repositories.UserSchema{}
 
-	return empty, nil
+	// Validate parameters request.
+
+	name, err := models.ValidateName(name)
+	if err != nil {
+		return empty, err
+	}
+
+	username, err = models.ValidateUsername(username)
+	if err != nil {
+		return empty, err
+	}
+
+	email, err = models.ValidateEmail(email)
+	if err != nil {
+		return empty, err
+	}
+
+	password, err = models.ValidatePassword(password)
+	if err != nil {
+		return empty, err
+	}
+
+	if strings.Trim(confirmation, " ") != password {
+		return empty, errors.New("Passwords did not match.")
+	}
+
+	// Hash the password before inserting.
+
+	hashedPassword, err := hashPassword(password)
+	if err != nil {
+		return empty, err
+	}
+
+	// Insert a new user to the repository & return the new user schema.
+
+	user, err := ga.UserRepo.CreateNewUser(name, username, email, hashedPassword)
+	if err != nil {
+		return empty, err
+	}
+
+	return user, nil
+}
+
+func hashPassword(password string) (string, error) {
+	cost := bcrypt.MinCost
+	secret := os.Getenv("PASSWORD_SECRET")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password+secret), cost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
 }
