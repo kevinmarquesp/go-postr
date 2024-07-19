1e185cbdb0144db6b68ec6ce844bc66170a54137
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-07-18 21:57:14 -0300

    :bug: fix (models): Now the validators return a trimmed version of the input string

--- internal/models/validators.go
+++ internal/models/validators.go
@@ -3,60 +3,62 @@ package models
 import (
 	"errors"
 	"regexp"
+	"strings"
 )
 
-func ValidateName(name string) error {
-	return nil
+func ValidateName(name string) (string, error) {
+	return strings.Trim(name, " "), nil
 }
 
-func ValidateUsername(username string) error {
+func ValidateUsername(username string) (string, error) {
 	if len(username) < 1 {
-		return errors.New("Empty username not allowed.")
+		return "", errors.New("Empty username not allowed.")
 
 	} else if regexp.MustCompile(`\s`).Match([]byte(username)) {
-		return errors.New("Username cannot have spaces included.")
+		return "", errors.New("Username cannot have spaces included.")
 
 	} else if !regexp.MustCompile(`^[a-zA-Z0-9_-]+$`).MatchString(username) {
-		return errors.New("Username could not have special characters other than - and _.")
+		return "", errors.New("Username could not have special characters other than - and _.")
+
 	}
 
-	return nil
+	return strings.Trim(username, " "), nil
 }
 
-func ValidateEmail(email string) error {
+func ValidateEmail(email string) (string, error) {
 	const regexMatchEmailString = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
 
 	if len(email) < 1 {
-		return errors.New("Empty email not allowed.")
+		return "", errors.New("Empty email not allowed.")
 
 	} else if !regexp.MustCompile(regexMatchEmailString).MatchString(email) {
-		return errors.New("Empty email not allowed.")
+		return "", errors.New("Empty email not allowed.")
 	}
 
-	return nil
+	return strings.Trim(email, " "), nil
 }
 
-func ValidatePassword(password string) error {
+func ValidatePassword(password string) (string, error) {
 	length := len(password)
 
 	if length == 0 {
-		return errors.New("Empty password not allowed.")
+		return "", errors.New("Empty password not allowed.")
 
 	} else if length < 12 {
-		return errors.New("Password is too short.")
+		return "", errors.New("Password is too short.")
 
 	} else if !regexp.MustCompile(`[A-Z]`).MatchString(password) {
-		return errors.New("The password should include uppercase letters.")
+		return "", errors.New("The password should include uppercase letters.")
 
 	} else if !regexp.MustCompile(`[a-z]`).MatchString(password) {
-		return errors.New("The password should include lowercase letters.")
+		return "", errors.New("The password should include lowercase letters.")
 
 	} else if !regexp.MustCompile(`[0-9]`).MatchString(password) {
-		return errors.New("The password should include numbers.")
+		return "", errors.New("The password should include numbers.")
 
 	} else if !regexp.MustCompile(`[!@#$%^&*()_+{}[\]|:;<>,.?/\\]`).MatchString(password) {
-		return errors.New("The password should include special characters.")
+		return "", errors.New("The password should include special characters.")
 	}
 
-	return nil
+	return strings.Trim(password, " "), nil
 }
