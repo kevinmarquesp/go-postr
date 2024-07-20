d5becc7e3f938dc0639bbffeabc618e961eab9de
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-07-19 21:16:53 -0300

    :construction: wip (repositories.sqlite-user): Created the basic structure with an query that I don't know if it will work...

---
+++ internal/repositories/sqlite_user_repository.go
@@ -0,0 +1,66 @@
+package repositories
+
+import (
+	"database/sql"
+	"errors"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type SqliteUserRepository struct {
+	UserRepository
+
+	database *sql.DB
+}
+
+func NewSqliteUserRepository(path string) (SqliteUserRepository, error) {
+	connection, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return SqliteUserRepository{}, err
+	}
+
+	return SqliteUserRepository{
+		database: connection,
+	}, nil
+}
+
+func (su SqliteUserRepository) CreateNewUser(id, name, username, email, password string) (UserSchema, error) {
+	empty := UserSchema{}
+
+	statement, err := su.database.Prepare(`
+      -- Insert the user data and the credentials information in different tables.
+
+      INSERT
+        INTO Users
+          (id, name, username, email, auth_type)
+      VALUES
+        (?1 ?2, ?3, ?4, "CREDENTIALS");
+
+      INSERT
+        INTO Credentials
+          (id, password)
+        VALUES
+          (?1, ?5);
+    `)
+	if err != nil {
+		return empty, err
+	}
+	defer statement.Close()
+
+	_, err = statement.Exec(id, name, username, email, password)
+	if err != nil {
+		return empty, err
+	}
+
+	return UserSchema{
+		Id:       id,
+		Name:     name,
+		Username: username,
+		Email:    email,
+		Password: password,
+	}, nil
+}
+
+func (su SqliteUserRepository) FindUniqueByEmail(email string) (UserSchema, error) {
+	return UserSchema{}, errors.New("Not implemented.")
+}
