bf7dc392afdc97b5cb4b3ddee32d08e26a16caf8
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-07-20 16:09:33 -0300

    :clipboard: impl (router): Using the new pages handler + some notes about future features

--- internal/router/router.go
+++ internal/router/router.go
@@ -32,11 +32,31 @@ func StartRouter(port string) error {
 	apiUserHandler := controllers.NewApiUserHandler(authenticationService)
 
 	apiRouter.HandleFunc("POST /user", apiUserHandler.RegisterNewUserWithCredentials)
-	apiRouter.HandleFunc("GET /user/{username}", apiUserHandler.FetchUserDataByUsername)
-	apiRouter.HandleFunc("PUT /user", apiUserHandler.UpdateProfileDetails)
-	apiRouter.HandleFunc("DELETE /user", apiUserHandler.RemoveRegisteredUser)
+	// apiRouter.HandleFunc("GET /user/{username}", apiUserHandler.FetchUserDataByUsername)
+	// apiRouter.HandleFunc("PUT /user", apiUserHandler.UpdateProfileDetails)
+	// apiRouter.HandleFunc("DELETE /user", apiUserHandler.RemoveRegisteredUser)
+	// apiRouter.HandleFunc("GET /users", apiUserHandler.FetchUsersList)
 
 	router := http.NewServeMux()
+
+	router.HandleFunc("/tailwind.min.css", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css")
+		http.ServeFile(w, r, "./dist/css/tailwind.min.css")
+	})
+
+	router.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./www/css"))))
+
+	pagesHandler := controllers.NewPagesHanlder()
+
+	router.HandleFunc("/", pagesHandler.RenderHomePage)
+	// router.HandleFunc("/u/{username}", pagesHandler.RenderProfilePage)
+	// router.HandleFunc("/publication/{id}", pagesHandler.RenderProfilePage)
+
+	// authHandler := constrollers.NewAuthHanlder()
+
+	// router.HandleFunc("/register", authHandler.RegisterNewUser)
+	// router.HandleFunc("/quit", authHandler.RegisterNewUser)
+	// router.HandleFunc("/delete", authHandler.RegisterNewUser)
 
 	router.Handle("/api/v1/", http.StripPrefix("/api/v1", apiRouter))
 
