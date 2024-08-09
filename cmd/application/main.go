4b859cd8b75ca68132165f87c234cc1982e8f132
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-08-08 21:44:26 -0300

    :beers: beer: Created a simple script to test the user repository behavior

--- cmd/application/main.go
+++ cmd/application/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/kevinmarquesp/go-postr/internal/models"
+	"github.com/kevinmarquesp/go-postr/internal/repositories"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	userRepo, err := repositories.NewUserSqliteRepository("tmp/application.db")
+	fatalOnError(err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+
+	password, err := hashPassword("Password123!")
+	fatalOnError(err)
+
+	props := repositories.CredentialsPropperties{
+		Role:     models.StandardRole,
+		Username: "fulano",
+		Email:    "me@email.com",
+		Password: password,
+	}
+
+	registerResult, err := userRepo.RegisterWithCredentials(ctx, props)
+	fatalOnError(err)
+
+	verifyResult, err := userRepo.VerifyAccount(ctx, registerResult.Id)
+	fatalOnError(err)
+
+	data, err := json.MarshalIndent(verifyResult, "", "  ")
+	fatalOnError(err)
+
+	fmt.Println(string(data))
+}
+
+func fatalOnError(err error) {
+	if err != nil {
+		fmt.Printf("\nFATAL!\n\t%s\n\n", err)
+		os.Exit(1)
+	}
+}
+
+func hashPassword(password string) (string, error) {
+	cost := bcrypt.MinCost
+	secret := os.Getenv("PASSWORD_SECRET")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password+secret), cost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
 }
